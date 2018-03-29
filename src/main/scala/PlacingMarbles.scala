d85703bbdb37b4572725aea1e6e6f5688a03edfb
masaki_kuwako <m.kuwako0702@gmail.com>
2018-03-30 01:23:56 +0900

    文字の一致がなぜかできない...

--- src/main/scala/PlacingMarbles.scala
+++ src/main/scala/PlacingMarbles.scala
@@ -1,10 +1,11 @@
 object PlacingMarbles extends App{
   val line = scala.io.StdIn.readLine()
 
-  var cnt = 0
-  if (line(0) == 1) cnt += 1
-  if (line(1) == 1) cnt += 1
-  if (line(2) == 1) cnt += 1
+  line.map(x => {
+    if (x == "0") println("a")
+    else if (x == 0) println("c")
+    else println("b")
+  })
 
-  println(cnt)
+  println(line.count({_ == "1"}).toString)
 }
