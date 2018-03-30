d5f0e94955c3cff68d163fb92949712c14407a3e
masaki_kuwako <m.kuwako0702@gmail.com>
2018-03-31 01:23:24 +0900

    scalaのcharとString勘違いしてた

--- src/main/scala/PlacingMarbles.scala
+++ src/main/scala/PlacingMarbles.scala
@@ -1,11 +1,10 @@
 object PlacingMarbles extends App{
   val line = scala.io.StdIn.readLine()
 
-  line.map(x => {
-    if (x == "0") println("a")
-    else if (x == 0) println("c")
-    else println("b")
-  })
+  var cnt = 0
+  if (line(0) == '1') cnt += 1
+  if (line(1) == '1') cnt += 1
+  if (line(2) == '1') cnt += 1
 
-  println(line.count({_ == "1"}).toString)
+  println(cnt)
 }
