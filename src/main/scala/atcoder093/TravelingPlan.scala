317cd9d28c24e3c9310c42aef0a75e2683fe0ca9
masaki_kuwako <m.kuwako0702@gmail.com>
2018-04-04 00:11:35 +0900

    TravelingPlanやっとAC

--- src/main/scala/atcoder093/TravelingPlan.scala
+++ src/main/scala/atcoder093/TravelingPlan.scala
@@ -5,31 +5,19 @@ object TravelingPlan extends App {
   import scala.math.abs
   val sc = new java.util.Scanner(System.in)
   val n = sc.nextInt
-  val A: Array[Int] = Array.fill(n)(sc.nextInt)
+  var A: Array[Int] = Array.fill(n)(sc.nextInt)
+  A = 0 +: A :+ 0
   // 全経路いった場合を計算
   var total = 0
-  for (i <- 0 until n) {
-    if (i == 0) total = abs(A(i))
-    else {
+  for (i <- 1 to n + 1) {
       total += abs(A(i) - A(i - 1))
-    }
   }
-  total += abs(A(n - 1))
 
   // 端折る箇所がi-1 i+1の間だった場合は全経路いった場合と同じ、違った場合はi と i-1の絶対値の2倍の額を合計から引く
-  for (i <- 0 until n) {
-    var before = 0
-    var after = 0
-    if (i != 0) before = A(i - 1)
-    if (i != (n - 1)) after = A(i + 1)
+  for (i <- 1 to n) {
+    val before = A(i - 1)
+    val after = A(i + 1)
 
-    if ((before <= A(i) & A(i) <= after) | (after <= A(i) & A(i) <= before)) {
-      println(total)
-    } else if (i == n - 1 & ((A(i - 2) > 0 & A(i - 1) < 0) | (A(i - 2) < 0 & A(i - 1) > 0))) {
-      // 最後の正負が違った場合の処理
-      println(total - 2 * abs(A(i) - before) + 2 * abs(A(i - 1)))
-    } else {
-      println(total - 2 * abs(A(i) - before))
-    }
+    println(total - abs(A(i) - before) - abs(after - A(i)) + abs(after - before))
   }
  }
