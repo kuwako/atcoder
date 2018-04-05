9437f58e259ea5796ec53e55d8c53e3d9db21e4e
masaki_kuwako <m.kuwako0702@gmail.com>
2018-04-05 23:36:55 +0900

    GridComponents

---
+++ src/main/scala/atcoder092/GridComponents.scala
@@ -0,0 +1,12 @@
+package atcoder092
+
+/**
+  * Created by mkuwako on 18/04/03.
+  */
+object GridComponents extends App {
+  import scala.math.abs
+  val sc = new java.util.Scanner(System.in)
+  val a = sc.nextInt
+  val b = sc.nextInt
+  
+ }
