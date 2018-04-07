b5f7607bc55520daa19ae77e53ac6cc6d6af4dca
masaki_kuwako <m.kuwako0702@gmail.com>
2018-04-08 00:04:21 +0900

    ABC093 D問題完

--- src/main/scala/atcoder093/WorstCase.scala
+++ src/main/scala/atcoder093/WorstCase.scala
@@ -3,19 +3,22 @@ package atcoder093
 object WorstCase extends App {
   val sc = new java.util.Scanner(System.in)
   val Q = sc.nextInt
-  val A: Array[Long] = Array.fill(2 * Q)(sc.nextInt)
 
   // int型でいけるか微妙?
   for (i <- 0 until Q) {
-    val a:Long = A(i * 2)
-    val b:Long = A(i * 2 + 1)
-    calc(a, b)
-  }
+    val a = sc.nextLong
+    val b = sc.nextLong
+    val m = a * b
+    val s :Long = (Math.sqrt(a * b)).toLong
 
-  def calc(a:Long, b:Long):Unit = {
-    val c: Long = a * b
-    var cnt = 0
+    var ans :Long = 2 * s - 1
 
-    // 4分円の格子点の数を数えたいが...方法がわからん..
+    if (s * (s + 1) >= m) {
+      ans -= 1
+    }
+    if (s * s == m & a != b) {
+      ans -= 1
+    }
+    println(ans)
   }
 }
