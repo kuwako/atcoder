2d93564aa4d6dc29eef805ab2a5ecfa89bc65be6
kuwako <m.kuwako0702@gmail.com>
2018-04-13 17:42:17 +0900

    C問題AC

--- src/main/scala/arc092/TwoDplaneTwoNPoints.scala
+++ src/main/scala/arc092/TwoDplaneTwoNPoints.scala
@@ -1,6 +1,5 @@
 package arc092
 
-import scala.util.control.Breaks
 
 /**
  * Created by mkuwako on 18/04/12.
@@ -8,12 +7,26 @@ import scala.util.control.Breaks
 object TwoDplaneTwoNPoints extends App {
   val sc = new java.util.Scanner(System.in)
   val n = sc.nextInt
-  val red = Array.fill(n, 2)(sc.nextInt)
-  val blue = Array.fill(n, 2)(sc.nextInt)
+  val red :List[List[Int]] = List.fill(n, 2)(sc.nextInt)
+  val blue :List[List[Int]] = List.fill(n, 2)(sc.nextInt)
+  val sortedRed :List[List[Int]] = red.sortWith((a, b) => a(1) > b(1))
+  val sortedBlue :List[List[Int]] = blue.sortBy(_(0))
 
   var cnt = 0
+  var usedList :List[Int] = Nil
 
-
+  sortedBlue.foreach(b => {
+    var loop = true
+    var index = 0
+    sortedRed.foreach(r => {
+      if (loop & !usedList.contains(index) & b(0) > r(0) & b(1) > r(1)) {
+        cnt += 1
+        loop = false
+        usedList = index :: usedList
+      }
+      index += 1
+    })
+  })
 
   println(cnt)
 }
