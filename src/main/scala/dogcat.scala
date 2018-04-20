21c1cc8bb70b3b1f2d6f15ee7704216c6d6e6de0
kuwako <m.kuwako0702@gmail.com>
2018-04-20 14:21:34 +0900

    paizaのコードが遅過ぎたので時間確認

--- src/main/scala/dogcat.scala
+++ src/main/scala/dogcat.scala
@@ -1,5 +1,8 @@
 object dogcat extends App {
+  val start = System.currentTimeMillis
+  val ct1 = System.currentTimeMillis
   val line1, line2, line3 = scala.io.StdIn.readLine
+  val interval = System.currentTimeMillis - ct1
   val lines: Array[String] = Array(line1, line2, line3)
 
   var dogCnt = 0
@@ -10,6 +13,7 @@ object dogcat extends App {
     else catCnt += 1
   })
 
-  if (dogCnt > 1) println("cat")
+  if (dogCnt > 1) println("dog")
   else println("cat")
+  println(System.currentTimeMillis - start - interval)
 }
