3b5a7f42773d53ecad813cf5cf93b15b1034e725
masaki_kuwako <m.kuwako0702@gmail.com>
2018-04-28 11:30:09 +0900

    検証コード追加

--- src/main/scala/algorithm/stack.scala
+++ src/main/scala/algorithm/stack.scala
@@ -13,17 +13,20 @@ object stack extends App {
     val c = line(i)
     if (c == '\\') s1.push(i)
     else if (c == '/' && s1.size > 0) {
+      println(i)
       val j = s1.pop
-      var a = i -j
+      var a = i - j // 溝の始点と終点の距離
       sum += a
       while(s2.size > 0 && s2.top._1 > j) {
         a += s2.pop._2
       }
       s2.push((j, a))
+      println(s2)
     }
   }
 
   println(sum)
+  println(s2)
   val s3 = s2.reverse.toList
   println(s2.size)
   s3.foreach(x => println(x._2))
