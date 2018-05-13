95696f2e653eee9d42d9e44f3b27b4ebfe814b19
masaki_kuwako <m.kuwako0702@gmail.com>
2018-05-13 13:55:03 +0900

    KthSubstring完

--- src/main/scala/arc097/KthSubstring.scala
+++ src/main/scala/arc097/KthSubstring.scala
@@ -4,12 +4,17 @@ object KthSubstring extends App {
   val sc = new java.util.Scanner(System.in)
   val s = sc.next
   val K = sc.nextInt
+  val b1 = new scala.util.control.Breaks
 
-  // 全通りやってソート...?
+  // 1 <= K <= 5だった...
   var list = List.empty[String]
+  val len = if (s.length > 5) 5 else s.length
   for (i <- 0 until s.length) {
-    for (j <- i + 1 to s.length) {
-      list = s.substring(i, j) :: list
+    b1.breakable {
+      for (j <- i + 1 to s.length) {
+        list = s.substring(i, j) :: list
+        if (j == i + 5) b1.break()
+      }
     }
   }
 
