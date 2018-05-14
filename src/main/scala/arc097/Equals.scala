0c6daad97c573b3e50f91b2d6fe9b8f3b3bcc1e1
masaki_kuwako <m.kuwako0702@gmail.com>
2018-05-14 23:28:12 +0900

    Equals

--- src/main/scala/arc097/Equals.scala
+++ src/main/scala/arc097/Equals.scala
@@ -1,48 +1,91 @@
 package arc097
 
+import java.io._
+import java.util.StringTokenizer
+import scala.util.control.Breaks.{breakable,break}
+
 object Equals extends App {
-  val N, M = MyPredef1.nextInt()
-  val p = Array.fill(N)(MyPredef1.nextInt())
-  val x = Array.fill(M, 2)(MyPredef1.nextInt())
+  val in = new InputReader(System.in)
+  val pw = new PrintWriter(System.out)
 
-  println(p.toList)
-  println(x.toList)
+  val N, M = in.next().toInt
+  val p = new Array[Int](N + 1)
+  val swaps = new Array[(Int, Int)](M)
 
+  for (i <- 1 to N) {
+    p(i) = in.next().toInt
+  }
+  for(i <- 0 until M){
+    val a, b = in.next().toInt
+    swaps(i) = (a, b)
+  }
+
+  val uf = new UnionFind(N + 1)
+
+  for ((a, b) <- swaps) {
+    uf.connect(a, b)
+  }
+
+  var ans = 0
+  for(i <- 1 to N) {
+    if(uf.isConnect(p(i), i))
+      ans += 1
+  }
+
+  println(ans)
 }
 
-object MyPredef1 {
-  @inline def rep(n: Int, f: => Unit): Unit = { var c = 0; while (c < n) { f; c += 1 } }
-  @inline def rep(n: Int, f: Int => Unit): Unit = { var c = 0; while (c < n) { f(c); c += 1 } }
 
-  private val buf = new Array[Byte](1024); private var ptr = 0; private var len = 0
-  @inline private def isPrintableChar(c: Int): Boolean = 33 <= c && c <= 126
-  @inline private def hasNextByte(): Boolean =
-    if (ptr >= len) { ptr = 0; len = System.in.read(buf); len > 0 } else { true }
-  @inline private def hasNext(): Boolean = {
-    while (hasNextByte() && !isPrintableChar(buf(ptr))) ptr += 1
-    hasNextByte()
+class UnionFind(v: Int){
+  // 根であれば *そのグループの要素数(負)* が、子であれば親の番号が入る。
+  val uni = Array.fill(v)(-1)
+
+  // 頂点 v の所属するグループを調べる
+  def root(v: Int): Int =
+    uni(v) < 0 match{
+      case true  =>  // v が親の場合
+        return v
+      case false =>  // v が子の場合
+        uni(v) = root(uni(v))  // 親のrootを調べる
+        return uni(v)
+    }
+
+  // 頂点 a と頂点 b をつなぐ。もともと同じグループのとき、False を返す
+  def connect(a: Int, b: Int): Boolean = {
+    // まずはそれぞれ根の番号に置き換える
+    var ra = root(a)
+    var rb = root(b)
+    if(ra == rb)
+      return false  // a と b がそもそも同じグループに属しているなら即終了
+
+    // ra を大きなグループにしたいので、逆であれば入れ替える
+    if(uni(ra) > uni(rb)) {  // rbの方が要素数が多ければ
+      var tmp = ra
+      ra = rb
+      rb = tmp
+    }
+
+    // ra と rb を結合し、rb の親を ra とする
+    uni(ra) += uni(rb)
+    uni(rb) = ra
+    return true
   }
-  @inline private def readByte(): Byte =
-    if (hasNextByte()) { val res = buf(ptr); ptr += 1; res } else { -1 }
+
+  // 頂点 a, b が同じグループであるかを調べる
+  def isConnect(a: Int, b: Int): Boolean =
+    root(a) == root(b)
+
+  // 頂点 a を含むグループの頂点数を調べる
+  def size(a: Int): Int = -uni(root(a))
+}
+
+class InputReader(stream: InputStream) {
+  private val reader = new BufferedReader(new InputStreamReader(stream))
+  private var tokenizer: StringTokenizer = new StringTokenizer(reader.readLine())
   def next(): String = {
-    if(!hasNext()) ???
-    val sb = new StringBuilder; var b = readByte()
-    while (isPrintableChar(b)) { sb.append(b.toChar); b = readByte() }
-    sb.toString
+    while (!tokenizer.hasMoreTokens()) {
+      tokenizer = new StringTokenizer(reader.readLine())
+    }
+    tokenizer.nextToken()
   }
-  def nextInt(): Int = {
-    val n = nextLong()
-    if (n < Int.MinValue || Int.MaxValue < n) ???
-    n.toInt
-  }
-  def nextLong(): Long = {
-    if(!hasNext()) ???
-    var minus = false; var b = readByte()
-    if (b == '-') { minus = true; b = readByte() }
-    def go (b: Byte, n: Long = 0): Long =
-      if ('0' <= b && b <= '9') { go(readByte(), n * 10 + b - '0') }
-      else if (minus) { -n } else { n }
-    go(b)
-  }
-  def nextDouble(): Double = next.toDouble
 }
