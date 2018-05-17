895d0f3205d0041113edc4b053fb512fa187f110
masaki_kuwako <m.kuwako0702@gmail.com>
2018-05-18 00:59:44 +0900

    LISDLできない

---
+++ src/main/scala/arc091/LISDL.scala
@@ -0,0 +1,38 @@
+package arc091
+
+object LISDL extends App {
+  val sc = new java.util.Scanner(System.in)
+  val N, A, B = sc.nextInt
+  val X = N - A - B
+
+  // A + B > N + 1 のとき実現不可能
+  if ((A + B) > (N + 1)) {
+    println(-1)
+    sys.exit
+  }
+
+  // X = N - A - B で A, BのどちらかがXより小さいと不可能
+  if (X > A | X > B) {
+    println(-1)
+    sys.exit
+  }
+
+  // それ以外のとき、実現可能
+  var arr = Array.emptyIntArray
+  if (B != N) {
+    for (i <- 1 to A) arr :+= i
+  }
+
+  if (A != N) {
+    for (j <- N - B + 1 to N) arr +:= j
+  }
+
+  if (A > B) {
+    for (k <- A + 1 to N - B) arr +:= k
+  } else {
+    for (k <- N - B to A + 1 by -1) arr +:= k
+  }
+
+
+  println(arr.mkString(" "))
+}
