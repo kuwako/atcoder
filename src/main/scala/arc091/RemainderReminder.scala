37e6155d07b4362df325ba3671f57b45f86f36e6
kuwako <m.kuwako0702@gmail.com>
2018-05-17 20:25:25 +0900

    Longにやられた

--- src/main/scala/arc091/RemainderReminder.scala
+++ src/main/scala/arc091/RemainderReminder.scala
@@ -1,5 +1,23 @@
 package arc091
 
 object RemainderReminder extends App {
+  val sc = new java.util.Scanner(System.in)
+  val N, K = sc.nextLong
 
+  var sum :Long = 0
+  if (K == 0) {
+    println(N * N)
+    sys.exit
+  }
+
+  for (b <- K + 1 to N) {
+    val x = N % b
+    if (x >= K) {
+      sum += (b - K) * (N / b) + x + 1 - K
+    } else {
+      sum += (b - K) * (N / b)
+    }
+  }
+
+  println(sum)
 }
