7fbbb8d41ca1eb41ec64bac35cf5794b5ace919a
masaki_kuwako <m.kuwako0702@gmail.com>
2018-06-02 01:04:56 +0900

    幅優先探索

--- src/main/cpp/algorithm/BreadthFirstSearch.cpp
+++ src/main/cpp/algorithm/BreadthFirstSearch.cpp
@@ -7,6 +7,31 @@ static const int INFTY = (1<<21);
 int n, M[N][N];
 int d[N];
 
+void bfs(int s)
+{
+    queue<int> q;
+    q.push(s);
+
+    for (int i = 0; i < n; ++i) d[i] = INFTY;
+    d[s] = 0;
+
+    int u;
+    while (!q.empty()) {
+        u = q.front(); q.pop();
+        for (int i = 0; i < n; ++i)
+        {
+            if (M[u][v] == o) continue;
+            if (d[v] != INFTY) continue;
+            d[v] = d[u] + 1;
+            q.push(v);
+        }
+    }
+    for (int i = 0; i < n; ++i)
+    {
+        cout << i+1 << " " << ((d[i] == INFTY) ? (-1) : d[i]) << endl;
+    }
+}
+
 int main()
 {
     int u, k, v;
@@ -25,4 +50,7 @@ int main()
             M[u][v] = 1;
         }
     }
+
+    bfs(0);
+    return 0;
 }
