9d13fd5d098336626aa0c3e2a8b12d3be12da9d7
masaki_kuwako <m.kuwako0702@gmail.com>
2018-06-09 17:27:44 +0900

    dfs書いた

--- src/main/cpp/algorithm/ConnectedComponents.cpp
+++ src/main/cpp/algorithm/ConnectedComponents.cpp
@@ -11,6 +11,19 @@ int color[MAX];
 
 void dfs(int r, int c)
 {
+    stack<int> S;
+    S.push(r);
+    color[r] = c;
+    while (!S.empty()) {
+        int u = S.top(); S.pop();
+        for (int i = 0; i < G[u]).size(); i++) {
+            int v = G[u][i];
+            if (color[v] == NIL) {
+                color[v] = c;
+                S.push(v);
+            }
+        }
+    }
 
 }
 
