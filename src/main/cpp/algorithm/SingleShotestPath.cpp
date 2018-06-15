94cc963b25ebde301ef21c133d2353664162011c
masaki_kuwako <m.kuwako0702@gmail.com>
2018-06-16 00:18:20 +0900

    単一始点最短経路_2

--- src/main/cpp/algorithm/SingleShotestPath.cpp
+++ src/main/cpp/algorithm/SingleShotestPath.cpp
@@ -1,1 +1,19 @@
-#include<>
+#include<iostream>
+using namespace std;
+static const int MAX = 100;
+static const int INFTY = INFTY = (1<<21);
+static const int WHITE = 0;
+static const int GRAY = 1;
+static const int BLACK = 2;
+
+int n, M[MAX][MAX];
+
+void dijkstra() 
+{
+}
+
+int main() 
+{
+    dijkstra();
+    return 0;
+}
