64703b371fe89e5bcc7aa0165f7c763ee252fc74
kuwako <m.kuwako0702@gmail.com>
2018-06-22 19:40:19 +0900

    Patisserie途中_2

--- src/main/cpp/abc100/Patisserie.cpp
+++ src/main/cpp/abc100/Patisserie.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 #include <algorithm>
 using namespace std;
 #define MAXN 1000
@@ -11,6 +12,16 @@ int main()
     for (int i = 1; i <= n; ++i)
     {
         scanf("%lld %lld %lld", &c[i][1], &c[i][2], &c[i][3]);
+    }
+
+    for (int i = 0; i < 8; i++) {
+        vector <long long> vec;
+        for (long long j = 1; j <= n; j++) {
+            long long s = 0;
+            for (long long k = 1; k <= 3; k++) {
+                
+            }
+        }
     }
 
 	return 0;
