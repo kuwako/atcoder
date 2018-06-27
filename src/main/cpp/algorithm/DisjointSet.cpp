20b51d211c9dacff6a6d9288ea80df9c2c0d82c8
masaki_kuwako <m.kuwako0702@gmail.com>
2018-06-27 22:35:54 +0900

    互いに素な集合_2

--- src/main/cpp/algorithm/DisjointSet.cpp
+++ src/main/cpp/algorithm/DisjointSet.cpp
@@ -10,4 +10,35 @@ class DisjointSet {
         p.resize(size, 0);
         for (int i = 0; i < n; ++i) makeSet(i);
     }
-}
+
+    void makeSet(int x) {
+        p[x] = x;
+        rank[x] = 0;
+    }
+
+    bool same(int x, int y) {
+        return findSet(x) == findSet(y);
+    }
+
+    void unit(int x, int y) {
+        link(findSet(x), findSet(y));
+    }
+
+    void link(int x, int y) {
+        if (rank[x] > rank[y]) {
+            p[y] = x;
+        } else {
+            p[x] = y;
+            if (rank[x] == rank[y]) {
+                rank[y]++;
+            }
+        }
+    }
+
+    int findSet(int x) {
+        if (x != p[x]) {
+            p[x] = findSet(p[x]);
+        }
+        return p[x];
+    }
+};
