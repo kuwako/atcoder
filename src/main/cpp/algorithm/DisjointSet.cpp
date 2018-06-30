58ad41674adcafc0ee1f1369714647e5ded0fe9b
masaki_kuwako <m.kuwako0702@gmail.com>
2018-06-30 17:31:17 +0900

    互いに素な集合_3

--- src/main/cpp/algorithm/DisjointSet.cpp
+++ src/main/cpp/algorithm/DisjointSet.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
-using namespase std;
+
+using namespace std;
 
 class DisjointSet {
     public: vector<int> rank, p;
@@ -8,7 +9,7 @@ class DisjointSet {
     DisjointSet(int size) {
         rank.resize(size, 0);
         p.resize(size, 0);
-        for (int i = 0; i < n; ++i) makeSet(i);
+        for (int i = 0; i < size; ++i) makeSet(i);
     }
 
     void makeSet(int x) {
@@ -42,3 +43,24 @@ class DisjointSet {
         return p[x];
     }
 };
+
+int main()
+{
+    int n, a, b, q;
+    int t;
+
+    cin >> n >> q;
+    DisjointSet ds = DisjointSet(n);
+
+    for (int i = 0; i < q; ++i)
+    {
+        cin >> t >> a >> b;
+        if (t == 0) ds.unit(a, b);
+        else if (t == 1) {
+            if (ds.same(a, b)) cout << 1 << endl;
+            else cout << 0 << endl;
+        }
+    }
+
+    return 0;
+}
