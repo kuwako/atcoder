c380e12e15dc57c0d1e4ca3abc3f6a2842fc4015
masaki_kuwako <m.kuwako0702@gmail.com>
2018-07-10 22:58:52 +0900

    領域探索3

--- src/main/cpp/algorithm/RangeTree.cpp
+++ src/main/cpp/algorithm/RangeTree.cpp
@@ -14,7 +14,7 @@ class Point {
     public:
         int id, x, y;
         Point() {}
-        Point(int, id, int x, int y): id(id), x(x), y(y) {}
+        Point(int id, int x, int y): id(id), x(x), y(y) {}
         bool operator < (const Point &p) const {
             return id < p.id;
         }
@@ -34,6 +34,34 @@ int np;
 
 bool lessX(const Point &p1, const Point &p2) { return p1.x < p2.x; }
 bool lessY(const Point &p1, const Point &p2) { return p1.y < p2.y; }
+
+int makeKDTree(int l, int r, int depth) {
+    if (!(l < r)) return NIL;
+    int mid = (l + r) / 2;
+    int t = np++;
+    if (depth % 2 == 0) {
+        sort(P + l, P + r, lessX);
+    } else {
+        sort(P + l, P + r, lessY);
+    }
+
+    T[t].location = mid;
+    T[t].l = makeKDTree(l, mid, depth + 1);
+    T[t].l = makeKDTree(mid + 1, r, depth + 1);
+
+    return t;
+}
+
+void find(int v, int sx, int tx, int sy, int ty, int depth, vector<Point> &ans) {
+    int x = P[T[v].location].x;
+    int y = P[T[v].location].y;
+
+    if (sx < x && x <= tx && sy <= y && y <= ty) {
+        ans.push_back(P[T[v].location]);
+    }
+
+    // TODO 
+}
 
 int main()
 {
