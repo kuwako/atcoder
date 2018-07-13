7bda833580a2921a980f44d01ff0b2f9b60063f3
kuwako <m.kuwako0702@gmail.com>
2018-07-13 18:53:28 +0900

    Saving snook

---
+++ src/main/cpp/soundhound2018summer/SavingSnook.cpp
@@ -0,0 +1,12 @@
+#include<cstdio>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+// https://soundhound2018-summer-qual.contest.atcoder.jp/tasks/soundhound2018_summer_qual_d
+int main()
+{
+
+
+    return 0;
+}
